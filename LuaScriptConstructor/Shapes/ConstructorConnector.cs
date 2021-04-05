2fdf37ac3fc2881ff30638dd4932014d72d77c03
Mihkail Glazatov <glazatov.mihail@gmail.com>
2021-04-05 17:50:13 +0700

    Created a connector class for the constructor

---
+++ LuaScriptConstructor/Shapes/ConstructorConnector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using Crainiate.Diagramming;
+
+namespace LuaScriptConstructor.Shapes
+{
+    /// <summary>
+    /// Represents a line in a diagram that connects diagram shapes.
+    /// </summary>
+    class ConstructorConnector : Connector
+    {
+        /// <summary>
+        /// Gets the port to which the start of the connector is connected.
+        /// </summary>
+        public Port StartPort
+        {
+            get
+            {
+                return Start.Port;
+            }
+        }
+
+        /// <summary>
+        /// Gets the port to which the end of the connector is connected.
+        /// </summary>
+        public Port EndPort
+        {
+            get
+            {
+                return End.Port;
+            }
+        }
+    }
+}
