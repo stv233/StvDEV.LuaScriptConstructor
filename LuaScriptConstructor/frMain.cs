083fdf65d244c5589ddc7142816c14f9ffc18bcb
Mihkail Glazatov <glazatov.mihail@gmail.com>
2021-04-05 17:51:21 +0700

    Set the initial UI

--- LuaScriptConstructor/frMain.cs
+++ LuaScriptConstructor/frMain.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using LuaScriptConstructor.Forms;
 
 namespace LuaScriptConstructor
 {
@@ -14,7 +8,95 @@ namespace LuaScriptConstructor
     {
         public frMain()
         {
-           
+            #region /// Initialization
+
+            #region /// Form
+
+            this.Size = new System.Drawing.Size(800, 600);
+
+            #endregion
+
+            #region /// Menu Strip
+
+            var msMain = new MenuStrip
+            {
+                Parent = this
+            };
+
+            #endregion
+
+            #region /// SplitContainer
+
+            var scMain = new SplitContainer
+            {
+                Width = this.ClientSize.Width,
+                Height = this.ClientSize.Height - msMain.Height,
+                SplitterDistance = this.ClientSize.Width / 10 * 4,
+                Left = 0,
+                Top = msMain.Height,
+                Parent = this
+            };
+
+            #region /// Palette
+
+            var cplMain = new ConstructorPalette
+            {
+                Width = scMain.Panel1.ClientSize.Width,
+                Height = scMain.Panel1.ClientSize.Height,
+                Parent = scMain.Panel1
+            };
+            cplMain.Tabs = new ConstructorPalette.ConstructorPaleteTabs(cplMain.Model);
+            cplMain.Model.Layers = cplMain.Tabs;
+            cplMain.Tabs[0].Name = "Basic";
+
+            #endregion
+
+            #region /// Tab Control
+
+            var tcMain = new TabControl
+            {
+                Width = scMain.Panel2.ClientSize.Width,
+                Height = scMain.Panel2.ClientSize.Height,
+                Parent = scMain.Panel2
+            };
+
+            var tpMain = new DiagramTabPage
+            {
+                Text = "Main",
+            };
+            tcMain.TabPages.Add(tpMain);
+
+            #endregion
+
+            #endregion
+
+            #endregion
+
+            #region /// Events
+
+            #region /// Resizes
+
+            scMain.Panel1.Resize += (s, e) =>
+            {
+                cplMain.Width = scMain.Panel1.ClientSize.Width;
+                cplMain.Height = scMain.Panel1.ClientSize.Height;
+            };
+
+            scMain.Panel2.Resize += (s, e) =>
+            {
+                tcMain.Width = scMain.Panel2.ClientSize.Width;
+                tcMain.Height = scMain.Panel2.ClientSize.Height;
+            };
+
+            this.Resize += (s, e) =>
+            {
+                scMain.Width = this.ClientSize.Width;
+                scMain.Height = this.ClientSize.Height - msMain.Height;
+            };
+
+            #endregion
+
+            #endregion
         }
     }
 }
