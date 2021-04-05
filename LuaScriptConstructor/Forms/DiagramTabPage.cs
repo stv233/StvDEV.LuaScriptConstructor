2d9bbea8ef56a51292def6f8bb3ed6d3c1564ccf
Mihkail Glazatov <glazatov.mihail@gmail.com>
2021-04-05 17:49:39 +0700

    Created a palette tab class for the constructor

---
+++ LuaScriptConstructor/Forms/DiagramTabPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LuaScriptConstructor.Forms
+{
+    /// <summary>
+    /// Diagram tab page.
+    /// </summary>
+    class DiagramTabPage : TabPage
+    {
+       
+
+        private ConstructorDiagram diagram;
+        public ConstructorDiagram Diagram
+        {
+            get
+            {
+                return diagram;
+            }
+            set
+            {
+                diagram = value;
+            }
+        }
+
+
+        public DiagramTabPage() : this("") { }
+
+        public DiagramTabPage(string name) : base(name)
+        {
+            diagram = new ConstructorDiagram
+            {
+                Width = this.ClientSize.Width,
+                Height = this.ClientSize.Height,
+                Parent = this
+            };
+
+            this.Resize += (s, e) =>
+            {
+                diagram.Width = this.ClientSize.Width;
+                diagram.Height = this.ClientSize.Height;
+            };
+        }
+    }
+}
