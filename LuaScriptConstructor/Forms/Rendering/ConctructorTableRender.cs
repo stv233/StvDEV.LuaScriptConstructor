9dc5f8d2d0e304cc93a243abe70e96d731ee36bb
Mihkail Glazatov <glazatov.mihail@gmail.com>
2021-04-05 17:50:59 +0700

    Created a table class for the constructor

---
+++ LuaScriptConstructor/Forms/Rendering/ConctructorTableRender.cs
@@ -0,0 +1,36 @@
+﻿using Crainiate.Diagramming;
+using Crainiate.Diagramming.Forms.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaScriptConstructor.Forms.Rendering
+{
+    class ConctructorTableRender : Crainiate.Diagramming.Forms.Rendering.TableRender
+    {
+
+        public override void RenderElement(IRenderable element, Graphics graphics, Render render)
+        {
+            base.RenderElement(element, graphics, render);
+
+            var table = element as Crainiate.Diagramming.Table;
+
+            if (table.Label != null)
+            {
+                LabelRender renderer = render.GetRenderer(table.Label) as LabelRender;
+                if (table.Label.Wrap)
+                {
+                    renderer.RenderElement(table.Label, graphics, render, table.InternalRectangle);
+                }
+                else
+                {
+                    renderer.RenderElement(table.Label, graphics, render);
+                }
+            }
+        }
+        
+    }
+}
---
+++ LuaScriptConstructor/Shapes/ConstructorTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Crainiate.Diagramming;
+
+namespace LuaScriptConstructor.Shapes
+{
+    /// <summary>
+    /// Constructor table.
+    /// </summary>
+    class ConstructorTable : Table
+    {
+        public ConstructorTable() : base() { }
+
+        public ConstructorTable(ConstructorTable prototype) : base(prototype) { }
+
+
+    }
+}
